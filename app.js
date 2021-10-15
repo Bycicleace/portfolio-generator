2585192126b2f5029eaf90f1c2afbe4517042dd9
kvammee <elliott.kvamme@gmail.com>
2021-10-14 19:58:04 -0500

    Created template and dynamic HTML

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
-const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
 
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-  console.log('================');
+fs.writeFile('index.html', generatePage(name,github), err => {
+  if (err) throw err;
 
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
+  console.log('Portfolio complete! Check out index.html to see the output!');
+})
